76cd5b0bc2265ef5adf9ead46e37b033bfb7a525
PowerShell Breakfast <58199755+spicyfeast@users.noreply.github.com>
2022-07-13 10:39:09 -0400

    Add files via upload
    Add Custom powershell prompt

---
+++ Custom PowerShell Prompt/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,33 @@
+function prompt {
+
+    #Assign Windows Title Text
+    $host.ui.RawUI.WindowTitle = "Current Folder: $pwd"
+
+    #Configure current user, current folder and date outputs
+    $CmdPromptCurrentFolder = Split-Path -Path $pwd -Leaf
+    $CmdPromptUser = [Security.Principal.WindowsIdentity]::GetCurrent();
+
+    # Test for Admin / Elevated
+    $IsAdmin = (New-Object Security.Principal.WindowsPrincipal ([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+
+    #Calculate execution time of last cmd and convert to milliseconds, seconds or minutes
+    $LastCommand = Get-History -Count 1
+    if ($lastCommand) { $RunTime = ($lastCommand.EndExecutionTime - $lastCommand.StartExecutionTime).TotalSeconds }
+
+    if ($RunTime -ge 60) {
+        $ts = [timespan]::fromseconds($RunTime)
+        $min, $sec = ($ts.ToString("mm\:ss")).Split(":")
+        $ElapsedTime = -join ($min, " min ", $sec, " sec")
+    }
+    else {
+        $ElapsedTime = [math]::Round(($RunTime), 2)
+        $ElapsedTime = -join (($ElapsedTime.ToString()), "s")
+    }
+
+    #Decorate the CMD Prompt
+    Write-Host "$elapsedTime " -NoNewline -ForegroundColor Magenta
+    Write-host ($(if ($IsAdmin) { 'Admin ' } else { '' })) -ForegroundColor Red -NoNewline
+    Write-Host "$($CmdPromptUser.Name) "  -ForegroundColor Green -NoNewline
+    Write-Host "$PWD" -ForegroundColor Cyan -NoNewline
+    return "> "
+} #end prompt function
